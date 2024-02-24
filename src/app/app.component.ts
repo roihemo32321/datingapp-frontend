0d00212ec0d77b0213f2f179afaeeb914e194480
Roie <roihemo32321@gmail.com>
2024-02-24 12:11:31 +0200

    Finish Section 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'dating-frontend';
+  users: any;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:5003/api/users').subscribe({
+      next: (response) => (this.users = response),
+      error: (err) => console.log(err),
+      complete: () => console.log('Request Completed'),
+    });
+  }
 }
