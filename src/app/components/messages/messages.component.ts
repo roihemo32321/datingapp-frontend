fcc96228f008478f3af5bcdf5f91830bfb7ce8c1
Roie <roihemo32321@gmail.com>
2024-03-26 19:19:56 +0200

    Added some styles for messages but need to improve caching and more.

--- src/app/components/messages/messages.component.ts
+++ src/app/components/messages/messages.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Message } from '../../models/message';
+import { Pagination } from '../../models/pagination';
+import { MessageService } from '../../services/message.service';
+import { PageEvent } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-messages',
   templateUrl: './messages.component.html',
-  styleUrl: './messages.component.scss'
+  styleUrl: './messages.component.scss',
 })
-export class MessagesComponent {
+export class MessagesComponent implements OnInit {
+  messages?: Message[];
+  pagination?: Pagination;
+  viewOptions = ['Unread', 'Inbox', 'Outbox'];
+  container = 'Inbox';
+  pageNumber = 1;
+  pageSize = 5;
 
+  constructor(private messageService: MessageService) {}
+
+  ngOnInit(): void {
+    this.loadMessages();
+  }
+
+  loadMessages() {
+    this.messageService
+      .getMessages(this.pageNumber, this.pageSize, this.container)
+      .subscribe((res) => {
+        if (res) {
+          this.messages = res.result;
+          this.pagination = res.pagination;
+        }
+      });
+  }
+
+  changePage($event: PageEvent) {
+    this.pageNumber = $event.pageIndex + 1;
+    this.pageSize = $event.pageSize;
+    this.loadMessages();
+  }
 }
